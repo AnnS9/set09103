223e82f85dab9e98856539bdd5b58e171831101b
Anna S <75726606+AnnS9@users.noreply.github.com>
2024-10-07 17:05:25 +0100

    Update hello.py
    changed to basic

--- hello.py
+++ hello.py
@@ -3,18 +3,10 @@ from flask import Flask
 app =  Flask(__name__)
 
 @app.route("/")
-def root() :
-    return "The default, 'root' route"
+def hello_world() :
+    return "Hello Napier"
 
 
 
-@app.route('/hello/')
-def hello_world():
-    return "Hello Napier :D"
 
-@app.route("/goodbye/")
-def goodbye():
-    return "Goodbye cruel world :("
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
